f6dc75b06500c1d1b780806e346bb99009a8f743
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-03-22 12:21:42 +0530

    20240321: Work on some examples on loop Fixes #1

---
+++ c/iteration/factorial.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n, fact = 1;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for (int i = n; i >= 1; i--)
+    {
+        fact = fact * i;
+    }
+    printf("The factorial of %d is %d.\n", n, fact);
+}
+
+/*
+    5! = 5 * 4 * 3 * 2 * 1  =   120
+    6! = 6 * 5 * 4 * 3 * 2 * 1  =   720
+
+    Limitation: Calculates factorial upto 12 only
+*/
