86a29cb6de9e89077e4f72b73c8ca6495a3936b0
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-03-23 12:13:15 +0530

    20240322: Create a menu-based program using switch-case Fixes #5

---
+++ c/decision/menu-based-calculator.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int ch, a, b, c;
+    while (1)
+    {
+        printf("\n\nCALCULATION PROGRAM\n\n");
+        printf("Choose one of the following:\n\n");
+        printf("1. Addition\n");
+        printf("2. Subtraction\n");
+        printf("\n\nEnter choice (0 to exit): [ ]\b\b");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 0:
+            printf("Bye!\n");
+            exit(0);
+            break;
+        case 1:
+            printf("\nEnter the first number: ");
+            scanf("%d", &a);
+            printf("Enter the second number: ");
+            scanf("%d", &b);
+            c = a + b;
+            printf("The sum is %d.\n", c);
+            break;
+        case 2:
+            printf("\nEnter the first number: ");
+            scanf("%d", &a);
+            printf("Enter the second number: ");
+            scanf("%d", &b);
+            c = a - b;
+            printf("The difference is %d.\n", c);
+            break;
+        default:
+            printf("\nInvalid option! Retry!\n\n");
+            break;
+        }
+    }
+    return 0;
+}
