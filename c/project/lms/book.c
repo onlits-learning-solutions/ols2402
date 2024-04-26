7133a0468781f8fc7297c12af9a9aa48bcc17f18
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-04-26 11:57:57 +0530

    Work on new book module #33

--- c/project/lms/book.c
+++ c/project/lms/book.c
@@ -2,6 +2,9 @@
 This module belongs to LMS project
 */
 #include <stdio.h>
+#include "common.c"
+
+#define FILENAME "book.dat"
 
 typedef struct book
 {
@@ -35,23 +38,29 @@ void newbook()
     scanf("%s", book.isbn);
     printf("Enter Price: ");
     scanf("%f", &book.price);
-    fp = fopen("lms.dat", "a");
+    fp = fopen(FILENAME, "a");
     fprintf(fp, "%d,%s,%s,%s,%s,%d,%s,%f\n", book.id, book.title, book.author, book.edition, book.publication, book.pages, book.isbn, book.price);
     fclose(fp);
-    printf("\nBook record saved!\n\n");    
+    printf("\nBook record saved!\n\n");
 }
 
 void getbooks()
 {
     BOOK book;
     FILE *fp;
-    fp = fopen("lms.dat", "r");
-    fscanf(fp, "%d,%s,%s,%s,%s,%d,%s,%f\n", &book.id, book.title, book.author, book.edition, book.publication, &book.pages, book.isbn, &book.price);
-    printf("%d\t%s\t%s\t%s\t%s\t%d\t%s\t%f\n", book.id, book.title, book.author, book.edition, book.publication, book.pages, book.isbn, book.price);
+    fp = fopen(FILENAME, "r");
+    printline(160);
+    printf("%-10s%-30s%-30s%-20s%-30s%-10s%-20s%-10s\n", "Book Id", "Title", "Author", "Edition", "Publication", "Pages", "ISBN", "Price");
+    printline(160);
+    for (int i = 1; i <= 3; i++)
+    {
+        fscanf(fp, "%d,%[^,],%[^,],%[^,],%[^,],%d,%[^,],%f\n", &book.id, book.title, book.author, book.edition, book.publication, &book.pages, book.isbn, &book.price);
+        printf("%-10d%-30s%-30s%-20s%-30s%-10d%-20s%-10.2f\n", book.id, book.title, book.author, book.edition, book.publication, book.pages, book.isbn, book.price);
+    }
+    printline(160);
     fclose(fp);
 }
 
 void getbook(int id)
 {
-
 }
