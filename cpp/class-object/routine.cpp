279869ab1bbf90bbecf52b1175b81d946a78b2bb
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-08-05 08:41:51 +0530

    20240805

---
+++ cpp/class-object/routine.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class period {
+    int id;
+    string day;
+    string time;
+    string subject;
+    string teacher;
+};
+
+class routine{
+    period periods[48];
+};
+
+int main(void)
+{
+
+    return 0;
+}
---
+++ java/nopackage/classobject/Routine.java
@@ -0,0 +1,14 @@
+class Period {
+    int id;
+    String day;
+    String time;
+    String subject;
+    String teacher;
+}
+
+public class Routine {
+    Period[] periods;
+    public static void main(String[] args) {
+
+    }
+}
