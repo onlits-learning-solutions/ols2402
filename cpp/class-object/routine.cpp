1eac332a90e71fc2c4997ec9789eb6412f3d3d0d
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-08-07 07:03:59 +0530

    20240807

--- cpp/class-object/routine.cpp
+++ cpp/class-object/routine.cpp
@@ -4,11 +4,33 @@ using namespace std;
 
 class period
 {
-    int id;
+    int id;                 //instance variable
     string day;
     string time;
     string subject;
     string teacher;
+
+    void setPeriod(int id, string day, string time, string subject, string teacher)
+    {
+        this->id = id;
+        this->day = day;
+        this->time = time;
+    }
+
+    void setId(int id)      //setter method
+    {
+        this->id = id;
+    }
+
+    int getId()             //getter method
+    {
+        return id;
+    }
+
+    void setDay(string day)
+    {
+        day = day;
+    }
 };
 
 class routine
@@ -16,9 +38,9 @@ class routine
     period periods; // periods is an object (static allocation)
     // period *periods = new period(); //here periods is not an object but it is pointer to object. (dynamic allocation)
 
-    void setdata()
+    void setRoutine()
     {
-        periods.id = 1;
+        
     }
 };
 
--- java/nopackage/classobject/Routine.java
+++ java/nopackage/classobject/Routine.java
@@ -4,14 +4,33 @@ class Period {
     private String time;
     private String subject;
     private String teacher;
+
+    void setPeriod(int id, String day, String time, String subject, String teacher) {
+        this.id = id;
+    }
+
+    void setId(int id) // setter method
+    {
+        this.id = id;
+    }
+
+    int getId() // getter method
+    {
+        return id;
+    }
 }
 
 public class Routine {
-    Period periods;     //here periods is just a reference variable not an object
-    
+    Period periods; // here periods is just a reference variable not an object
+
     void setRoutine() {
-        periods.id = 1;
+
     }
+
+    void setDay(String day) {
+        day = day;
+    }
+
     public static void main(String[] args) {
 
     }
