48fe788a5bce4ac796971e59f1e0bc512a016203
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-09-22 06:03:55 +0530

    20240920

--- c/array/selection-sort.c
+++ c/array/selection-sort.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
-#define SIZE 5          
+#define SIZE 5
 
-int main(void)                          //O(n^2)
+int main(void) // O(n^2)
 {
-    int nums[SIZE] = {52, 1, 42, 19, 26};       //O(1)
-    
-    printf("Unsorted Array!\n");                //O(1)
-    for (int i = 0; i < SIZE; i++)              //O(n)
+    int nums[SIZE] = {52, 1, 42, 19, 26}; // O(1)
+
+    printf("Unsorted Array!\n");   // O(1)
+    for (int i = 0; i < SIZE; i++) // O(n)
     {
         printf("%d\n", nums[i]);
     }
 
-    for (int i = 0; i < SIZE - 1; i++)                  //O(n)
+    for (int i = 0; i < SIZE - 1; i++) // O(n)
     {
-        for (int j = i + 1; j < SIZE; j++)              // O(n) x O(n)  =   O(n^2)
-        {           
+        for (int j = i + 1; j < SIZE; j++) // O(n) x O(n)  =   O(n^2)
+        {
             if (nums[j] > nums[i])
             {
                 int t = nums[j];
@@ -25,8 +25,11 @@ int main(void)                          //O(n^2)
     }
 
     printf("Sorted Array!\n");
-    for (int i = 0; i < SIZE; i++)      //O(n)
+    for (int i = 0; i < SIZE; i++) // O(n)
     {
         printf("%d\n", nums[i]);
     }
 }
+
+// Time Complexity  =   O(n^2)
+// Space Complexity =   O(1)
