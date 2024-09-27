27b4032eaa2117a9f8ade1b8c548dc1ffa60c10a
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-09-27 07:30:40 +0530

    Array Operations Fixes #86

--- dsa/c/array/array-operations.c
+++ dsa/c/array/array-operations.c
@@ -6,16 +6,21 @@
 
 void drawLine(int n);
 void messageBox(char *message);
-void display(int *arr, int size);
+void display(int const *arr, int size);
 void insertBeginning(int *arr, int *size);
 void insertEnd(int *arr, int *size);
 void insert(int *arr, int *size);
+void deleteBeginning(int *arr, int *size);
+void deleteEnd(int *arr, int *size);
+void delete(int *arr, int *size);
+void linearSearch(int const *arr, int size);
+void binearSearch(int const *arr, int size);
 
 int main(int argc, char const *argv[])
 {
-    int arr[MAX], size = 0;
+    int arr[MAX], size = 0, sorted = 0;
     int ch;
-    int linesize = strlen("2. Insert element at the beginning");
+    int linesize = strlen("7. Delete element at the given index");
 
     while (1) // O(~)
     {
@@ -25,6 +30,12 @@ int main(int argc, char const *argv[])
         printf("2. Insert element at the beginning\n");
         printf("3. Insert element at the end\n");
         printf("4. Insert element at a given index\n");
+        printf("5. Delete element from beginning\n");
+        printf("6. Delete element from the end\n");
+        printf("7. Delete element at the given index\n");
+        printf("8. Search (using linear search)\n");
+        printf("9. Search (using binary search)\n");
+
         drawLine(linesize);
         printf("\nEnter choice [0 to exit]: ");
         scanf("%d", &ch);
@@ -43,6 +54,21 @@ int main(int argc, char const *argv[])
             break;
         case 3:
             insertEnd(arr, &size);
+            break;
+        case 4:
+            insert(arr, &size);
+            break;
+        case 5:
+            deleteBeginning(arr, &size);
+            break;
+        case 6:
+            deleteEnd(arr, &size);
+            break;
+        case 7:
+            delete (arr, &size);
+            break;
+        case 8:
+            linearSearch(arr, size);
             break;
         default:
             messageBox("Invalid Option!");
@@ -69,13 +95,14 @@ void messageBox(char *message) // O(1)
     printf("\n");
 }
 
-void display(int *arr, int size) // O(n)
+void display(int const *arr, int size) // O(n)
 {
     if (size == 0)
     {
         messageBox("Array empty!");
         return;
     }
+
     printf("\nArray Content:\n");
     drawLine(strlen("Array Content:"));
     printf("\n");
@@ -122,7 +149,7 @@ void insertEnd(int *arr, int *size) // O(1)
     messageBox("Value inserted!");
 }
 
-void insert(int *arr, int *size)
+void insert(int *arr, int *size) // O(n)
 {
     int index;
     if (*size >= MAX)
@@ -134,6 +161,12 @@ void insert(int *arr, int *size)
     printf("Enter index: ");
     scanf("%d", &index);
 
+    if (index > *size || index < 0)
+    {
+        messageBox("Not a valid index!");
+        return;
+    }
+
     for (int i = *size; i > index; i--)
         arr[i] = arr[i - 1];
 
@@ -143,4 +176,95 @@ void insert(int *arr, int *size)
     (*size)++;
 
     messageBox("Value inserted!");
+}
+
+void deleteBeginning(int *arr, int *size) // O(n)
+{
+    if (*size == 0)
+    {
+        messageBox("Array empty!");
+        return;
+    }
+
+    for (int i = 0; i < *size - 1; i++)
+        arr[i] = arr[i + 1];
+
+    (*size)--;
+
+    messageBox("Value removed!");
+}
+
+void deleteEnd(int *arr, int *size) // O(1)
+{
+    if (*size == 0)
+    {
+        messageBox("Array empty!");
+        return;
+    }
+
+    (*size)--;
+
+    messageBox("Value removed!");
+}
+
+void delete(int *arr, int *size) // O(n)
+{
+    int index;
+
+    if (*size == 0)
+    {
+        messageBox("Array empty!");
+        return;
+    }
+
+    printf("Enter index: ");
+    scanf("%d", &index);
+
+    if (index > *size || index < 0)
+    {
+        messageBox("Not a valid index!");
+        return;
+    }
+
+    for (int i = index; i < *size - 1; i++)
+        arr[i] = arr[i + 1];
+
+    (*size)--;
+
+    messageBox("Value removed!");
+}
+
+void linearSearch(int const *arr, int size)
+{
+    int key, index = -1;
+    if (size == 0)
+    {
+        messageBox("Array empty!");
+        return;
+    }
+
+    printf("Enter the value to search: ");
+    scanf("%d", &key);
+
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == key)
+        {
+            index = i;
+            break;
+        }
+    }
+
+    if (index == -1)
+        messageBox("Value not found!");
+    else
+    {
+        char *strIndex;
+        sprintf(strIndex, "%d", index);
+        messageBox(strcat("Value found at index", strIndex));
+    }
+}
+
+void binearSearch(int const *arr, int size)
+{
 }
