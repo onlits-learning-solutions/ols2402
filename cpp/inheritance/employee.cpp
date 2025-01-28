e65b19e704e351056635c565aaead4c056d79b63
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2025-01-28 08:29:56 +0530

    Linklist operations Fixes #88

--- cpp/inheritance/employee.cpp
+++ cpp/inheritance/employee.cpp
@@ -6,79 +6,92 @@ namespace employee
 {
     class Employee // Base class
     {
-    private:
+    protected:
         int empId;
         string name;
         string designation;
-
-    public:
-        Employee(/* args */); // Constructor
-        ~Employee();          // Destructor
     };
 
-    Employee::Employee(/* args */)
-    {
-    }
-
-    Employee::~Employee()
-    {
-    }
-
-    class Manager // Derived class
+    class Manager : Employee // Derived class
     {
     private:
-        int attendance;     //instance variable
+        int attendance; // instance variable
         float salary;
 
     public:
-        Manager() {}                           // Default Constructor
-        Manager(int attendance, float salary); // Parameterized Constructor
+        Manager() {}                                                                       // default constructor
+        Manager(int empId, string name, string designation, int attendance, float salary); // Parameterized Constructor
         ~Manager();
         void display();
     };
 
-    Manager::Manager(int attendance, float salary)      //local variable
+    Manager::Manager(int empId, string name, string designation, int attendance, float salary) // local variable
     {
+        this->empId = empId;
+        this->name = name;
+        this->designation = designation;
         this->attendance = attendance;
         this->salary = salary;
     }
 
     Manager::~Manager()
     {
+        cout << "Manager object deleted!" << empId << endl;
     }
 
     void Manager::display()
     {
+        cout << "Id: " << empId << endl;
+        cout << "Name: " << name << endl;
+        cout << "Designation: " << designation << endl;
         cout << "Attendance: " << attendance << endl;
         cout << "Salary: " << salary << endl;
     }
 
-    class Worker // Derived class
+    class Worker : Employee // Derived class
     {
     private:
         int hours; // only round numbers allowed a worker can work for 1 hour or 2 hours but not 1.5 hours.
         float wage;
 
     public:
-        // Worker(/* args */);
+        Worker() {}
+        Worker(int empId, string name, string designation, int hours, float wage);
         ~Worker();
+        void display();
     };
 
-    // Worker::Worker(/* args */)
-    // {
-    // }
+    Worker::Worker(int empId, string name, string designation, int hours, float wage)
+    {
+        this->empId = empId;
+        this->name = name;
+        this->designation = designation;
+        this->hours = hours;
+        this->wage = wage;
+    }
+
+    void Worker::display()
+    {
+        cout << "Employee Id: " << empId << endl;
+        cout << "Name: " << name << endl;
+        cout << "Designation: " << designation << endl;
+        cout << "Hours: " << hours << endl;
+        cout << "Wage: " << wage << endl;
+    }
 
     Worker::~Worker()
     {
+        cout << "Worker object deleted!" << empId << endl;
     }
 }
 
 int main(int argc, char const *argv[])
 {
-    employee::Manager m1(21, 15000.00F), m2(23, 16000.00F);
-    employee::Manager m3;
-    employee::Worker w1, w2;
+    employee::Manager m1(1, "Santosh Kumar", "Accounts Manager", 21, 15000.00F), m2(2, "Raju Kumar", "Sales Manager", 23, 16000.00F);
+    employee::Worker w1(3, "Ram Parvesh", "Fitter", 200, 10000.00F), w2(4, "Mohan Kumar", "Loom Operator", 220, 11000.00F);
     m1.display();
     m2.display();
+    w1.display();
+    w2.display();
     return 0;
 }
